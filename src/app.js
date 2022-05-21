286a8d60821f3418748fcda5b76705f608c969f7
Dmitry Votchitsev <dmitry.votchitsev@gmail.com>
2022-05-21 08:33:55 +0200

    task complete

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const characters = [
+  { name: 'Мечник', health: 10 },
+  { name: 'Маг', health: 100 },
+  { name: 'Маг', health: 0 },
+  { name: 'Лучник', health: 0 },
+];
+
+const alive = characters.filter((item) => item.health > 0);
+console.log(alive);
